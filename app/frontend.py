451800cd4cc7910de34a8e38d4f07382d6d7df2f
soulasphyxia <maslyuk03@bk.ru>
2023-12-13 21:38:21 +0700

    initial commit

--- app/frontend.py
+++ app/frontend.py
@@ -2,17 +2,21 @@ import streamlit as st
 import requests
 
 
+
+
 def main():
     st.title('Streamlit Frontend for Flask API')
     st.button(label ="click on me",on_click = button_on_click())
+    
 
 
 
 def button_on_click():
-    response = requests.get('http://localhost:5000/api/data')
+    response = requests.get(url="http://localhost:5000/api/data")
     data = response.json()
     
-    st.write(f"Random number\n: {data['data']}")
+    cost = data["data"]
+    st.write(f"random:{cost}")
     
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template,request
 import streamlit as st
 from app import analysis
 
-@app.route("/api/data")
+@app.route("/api/data",methods=['GET'])
 def index():
-    data = analysis.get_num()
-    return {'data': data}
+    current = request.args.get("current")
+    target = request.args.get("target")
+    
+    cost = analysis.get_num()
+    return {
+        "data": cost
+    }
 
 
