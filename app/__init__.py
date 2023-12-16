132927ec127b20a98137432d1bc410990b638f87
soulasphyxia <maslyuk03@bk.ru>
2023-12-17 05:20:42 +0700

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 from app import routes
